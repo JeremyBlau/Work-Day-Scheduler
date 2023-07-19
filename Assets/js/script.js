346c3c1c7da14a00ed89d23f991e25f54a758b28
jwblau2 <jeremy@abundantly.com>
2023-07-19 00:06:48 -0600

    added current time to the top of the page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,15 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Function to update the current time
+function updateCurrentTime() {
+  const currentDayElement = document.getElementById('currentDay');
+  const currentTime = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+  currentDayElement.textContent = currentTime;
+}
+
+// Call the function initially
+updateCurrentTime();
+
+// Update the current time every second
+setInterval(updateCurrentTime, 1000);
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
