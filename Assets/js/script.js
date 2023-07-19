8c2f9afddc238c40188f21046fe178916ae167c2
jwblau2 <jeremy@abundantly.com>
2023-07-19 00:25:58 -0600

    added a function to retrieve the current hour

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -37,14 +37,22 @@ for (var i = 1; i <= 24; i++) {
     $('#' + timeBlockId + ' .description').val(savedValue);
   }
 }
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+$(function() {
+  // Retrieve the current hour
+  var currentHour = dayjs().format('H');
+
+  // Add a click event listener to all save buttons
+  var saveButtons = document.querySelectorAll('.saveBtn');
+  saveButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      // Get the id of the parent time-block
+      var timeBlockId = this.parentNode.id;
+
+      // Get the user input from the textarea within the time-block
+      var userInput = this.parentNode.querySelector('.description').value;
+
+      // Save the user input in local storage using the time-block id as the key
+      localStorage.setItem(timeBlockId, userInput);
+    });
+  });
 });
